bbd751a0b4f056087020cbc6ff4e3d56f100be80
Hunter Welch <hjw36@case.edu>
2023-02-08 16:59:50 -0500

    Finished q2a working on q2b

--- A2/A2a/noisy_gaussian.py
+++ A2/A2a/noisy_gaussian.py
@@ -28,9 +28,103 @@ def genwaveform(N=100, alpha=0.1, A=1, sigma=1, noisetype="Gaussian"):
 def plot_genwaveform(N=100, alpha=0.1, A=1, sigma=1, noisetype="Gaussian"):
     waveform, event_indices = genwaveform(N, alpha, A, sigma, noisetype)
     plt.plot(waveform)
-    plt.scatter(event_indices, np.ones_like(event_indices) * A * waveform[event_indices], marker='x', color='red', label='events')
+    plt.scatter(event_indices, np.ones_like(event_indices) * waveform[event_indices], marker='x', color='red', label='events')
     plt.legend()
     plt.grid(color='gray', linewidth=0.5, linestyle='--')
     plt.title("Randomly occuring events in Gaussian Noise")
     plt.show()
 
+
+# compare the parameters
+def plot_comparison():
+    fig, axs = plt.subplots(2, 2)
+    waveform, event_indices = genwaveform(A=1, noisetype="Gaussian")
+    axs[0, 0].plot(waveform)
+    axs[0, 0].scatter(event_indices, np.ones_like(event_indices) * waveform[event_indices], marker='x', color='red', label='events')
+    axs[0, 0].set_title('A=1, noisetype=Gaussian')
+
+    waveform, event_indices = genwaveform(A=100, noisetype="Gaussian")
+    axs[0, 1].plot(waveform, 'tab:orange')
+    axs[0, 1].scatter(event_indices, np.ones_like(event_indices) * waveform[event_indices], marker='x', color='red', label='events')
+    axs[0, 1].set_title('A=100, noisetype=Gaussian')
+
+
+    waveform, event_indices = genwaveform(A=1, noisetype="uniform")
+    axs[1, 0].plot(waveform, 'tab:green')
+    axs[1, 0].scatter(event_indices, np.ones_like(event_indices) * waveform[event_indices], marker='x', color='red', label='events')
+    axs[1, 0].set_title('A=1, noisetype=uniform')
+
+    waveform, event_indices = genwaveform(A=100, noisetype="uniform")
+    axs[1, 1].plot(waveform, 'tab:red')
+    axs[1, 1].scatter(event_indices, np.ones_like(event_indices) * waveform[event_indices], marker='x', color='red', label='events')
+    axs[1, 1].set_title('A=100, noisetype=uniform')
+    fig.tight_layout()
+    plt.show()
+
+
+
+# going to have this return an array of indexes instead for now, will be more useful
+def detectioncounts(si, y, theta):
+    tp, fn, fp, tn = 0, 0, 0, 0
+    ele = 0
+    while ele < len(y):
+        # if element index is a signal
+        if ele in si:
+            if y[ele] >= theta:
+                # then the element should be greater than theta i.e. true positive
+                tp += 1
+            else:
+                # if not then it is a false positive
+                fp += 1
+
+        # if the element is not a signal            
+        else:
+            if y[ele] >= theta:
+                # if this is greater than or equal to theta but it is not a signal it is a false negative
+                fn += 1
+            else:
+                # otherwise it is a true negative
+                tn += 1
+        ele += 1
+    return [tp, fn, fp, tn]
+
+
+
+
+def plot_detection_types(si, y, theta, title="Graph with detection types"):
+
+    plt.plot(y)
+    plt.plot(theta, "r")
+    # plot the tp, fp, and fn
+    ele = 0
+    while ele < len(y):
+        # if element index is a signal
+        if ele in si:
+            if y[ele] >= theta:
+                # then the element should be greater than theta i.e. true positive
+                plt.scatter(ele, y[ele], "b.", label="True Positives")
+            else:
+                # if not then it is a false positive
+                plt.scatter(ele, y[ele], "g^", label="False Positives")
+
+        # if the element is not a signal            
+        else:
+            if y[ele] >= theta:
+                # if this is greater than or equal to theta but it is not a signal it is a false negative
+                plt.scatter(ele, y[ele], "o,", label="False Negatives")
+        ele += 1
+        
+    plt.title(title)
+
+
+si = [0, 4]
+y = [0, 1, 2, 3, 4, 5, 6]
+theta = 2
+plot_detection_types(si, y, theta)
+# tp - 4
+# fn - 2, 3, 5, 6
+# fp - 0
+# tn, 1
+
+# plot_genwaveform()
+# plot_comparison()
