56e03edeaec93c4d511ebc8883e64f8446bb668e
Hunter Welch <hjw36@case.edu>
2023-02-18 16:25:52 -0500

    Added questions 3a and 3b

--- A2/A2b/dim_flash.py
+++ A2/A2b/dim_flash.py
@@ -9,7 +9,7 @@ def randtimes(N, t1, t2):
 def plotflash(N, t1, t2):
     x = randtimes(N, t1, t2)
     y = np.ones(N)
-    plt.stem(x, y, markerfmt=' ')
+    plt.stem(x, y, markerfmt=' ', basefmt=' ')
     plt.title(f'Plot of {N} random times from [{t1}, {t2})')
     plt.show()
 
@@ -29,7 +29,7 @@ def plotrandintervals(N, l, t1):
     x = randintervals(N, l, t1)
     print(N, len(x))
     y = np.ones(N)
-    plt.stem(x, y, markerfmt=' ')
+    plt.stem(x, y, markerfmt=' ', basefmt=' ')
     plt.title(f'Plot of {N} random times starting at {t1} at a rate of {l}')
     plt.show()
 
@@ -69,4 +69,46 @@ def plotdetectionprob(K, l=40, T=0.1):
     plt.title(f'CDF of {K-1} events at rate {l} and time interval {T}')
     plt.show()
 
-plotdetectionprob(10)
+
+def lightflash(l, t1=0.8, t2=2.2):
+    y = [t1]
+    t_x = t1
+    while t_x < t2:
+        t_x = t_x + np.random.exponential(1/l)
+        y.append(t_x)
+    return np.array(y)
+
+
+def plotlightflash(l, s1=1, s2=2):
+    fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(6, 8))
+
+    x1 = lightflash(l)
+    y1 = np.ones(len(x1))
+    axs[0].stem(x1, y1, markerfmt=' ', basefmt=' ')
+    axs[0].set(title=f'Photon stream at a rate of {l} photons / msec')
+
+    x2 = []
+    for x in x1:
+        if x >= s1 and x <= s2:
+            x2.append(x)
+    x2 = np.array(x2)
+    y2 = np.ones(len(x2))
+    axs[1].stem(x2, y2, markerfmt=' ', basefmt=' ')
+    axs[1].set(title=f'Photons that pass through a shutter')
+
+    x3 = np.random.choice(x2, size=int(len(x2)*0.06), replace=False)
+    y3 = np.ones(len(x3))
+    axs[2].stem(x3, y3, markerfmt=' ', basefmt=' ')
+    axs[2].set(title=f'Photons that are finally detected.', xlabel='ms')
+
+    for ax in axs:
+        ax.set_xlim([x1[0], x1[len(x1) - 1]])
+
+    plt.tight_layout()
+    plt.show()
+
+
+def probseeing(I, a=0.06, K=6):
+    return 1 - poisson.cdf(K-1, I*a)
+
+probseeing(1000)
